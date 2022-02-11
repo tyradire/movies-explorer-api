91ffe3d8d39258e8801d219b61f465d678c3e960
Daniel Ugarov <tyradire@yandex.ru>
2022-02-11 15:51:34 +0300

    movieRouter post regex del

--- routes/movies.js
+++ routes/movies.js
@@ -18,10 +18,8 @@ movieRouter.post('/movies', celebrate({
     trailer: Joi.string().required().custom(urlValidator),
     thumbnail: Joi.string().required().custom(urlValidator),
     movieId: Joi.number().required().min(1).max(1000),
-    nameRU: Joi.string().required().min(2).max(100)
-      .regex(/^[А-Яа-яёЁ.^*%$#!~@—'"`,-\w\s]+$/),
-    nameEN: Joi.string().required().min(2).max(100)
-      .regex(/^[\w.^*%$#!~@—'"`,-\d\s]+$/),
+    nameRU: Joi.string().required().min(2).max(100),
+    nameEN: Joi.string().required().min(2).max(100),
   }),
 }), createMovie);
 
